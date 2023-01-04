a6a4a52d2a08901cfd5748274c7b020756716846
Imran Rasheed <imranrasheed.developer@outlook.com>
2023-01-04 17:26:40 +0500

    intial commit

---
+++ policy.js
@@ -0,0 +1,51 @@
+document.querySelector('.policy .container .content').innerText = 
+
+`
+At Rizorsi Umani Ltd., we are committed to protecting the privacy of our users. This Privacy Policy explains how we collect, use, and share information about you when you use our digital platform and related services (collectively, the "Services"). By using the Services, you agree to the collection, use, and sharing of your information as described in this Privacy Policy.
+
+1. Information We Collect
+We collect information about you when you use the Services. This includes:
+
+i) Personal information you provide to us: We may collect personal information about you, such as your name, email address, phone number, and address, when you create an account, contact us, or otherwise interact with the Services.
+ii) Information about your use of the Services: We may collect information about your use of the Services, such as the pages/apps you visit, the features you use, and the searches you conduct.
+iii) Device information: We may collect information about the devices you use to access the Services, such as your IP address, device type, and operating system.
+
+2. How We Use Your Information
+We use the information we collect about you to provide, maintain, and improve the Services. Specifically, we may use your information to:
+
+i)  Provide the Services: We use your information to provide the Services to you, including to process your requests for temporary employees and to communicate with you about the Services.
+ii)  Analyze and improve the Services: We may use your information to analyze and improve the Services, including to understand how the Services are used and to personalize your experience.
+iii)  Communicate with you: We may use your information to communicate with you about the Services, including to send you updates, announcements, and other information.
+
+
+3. Sharing Your Information
+We may share your information with third parties for the following purposes:
+
+i) Service providers: We may share your information with service providers who perform services on our behalf, such as partners, temping employees, hosting, payment processing, and analytics.
+ii) Legal purposes: We may share your information if we believe it is necessary to comply with a legal obligation, to protect the safety, rights, or property of ourselves or others, or to enforce the terms of our Services.
+iii) Business transfers: In the event that we sell or transfer all or a portion of our business, we may share your information with the buyer or transferee as part of that transaction.
+
+4. Your Choices and Rights
+You have the following choices and rights regarding your information:
+
+i) Access and correction: You can access and correct your personal information by logging into your account or contacting us.
+ii) Deletion: You can request that we delete your personal information by contacting us. Please note that we may be required to retain some of your information for legal purposes or to fulfill our contractual obligations.
+iii) Opt-out: You can opt-out of certain uses of your information, such as marketing communications, by following the instructions in those communications or by contacting us.
+iv) Objection: You can object to the processing of your information for certain purposes, such as for marketing or for research and development, by contacting us.
+v) Restriction: You can request that we restrict the processing of your information in certain circumstances, such as if you contest the accuracy of the information or if you object to the processing.
+
+Portability: You can request a copy of your personal information in a structured, commonly used, and machine-readable format, and you can request that we transmit that information to another organization, by contacting us.
+
+5. Data Security
+We take reasonable measures to protect your information from unauthorized access, use, or disclosure. However, no security measures are perfect, and we cannot guarantee the security of your information.
+
+6. Data Retention
+We retain your information for as long as necessary to provide the Services, or as required by law.
+
+7. Changes to this Privacy Policy
+We may update this Privacy Policy from time to time. We will post any changes on this page and encourage you to review the Privacy Policy periodically. If we make significant changes, we may also provide additional notice,
+such as by sending you an email or displaying a notice within the Services.
+
+8.	Contact Us
+If you have any questions or concerns about this Privacy Policy or our handling of your information, please contact us at +356 99722009 or via post at Stall no. 13, Best Buy Market, Market Street, Victoria, Gozo.
+`
---
+++ terms.js
@@ -0,0 +1,55 @@
+document.querySelector('.terms .container .content').innerText =
+
+`
+Effective from: Thursday - 14/07/2022
+
+1. INTRODUCTION
+Welcome to Rizorsi Umani Ltd. ("Rizorsi", "we", "us", or "our"). Rizorsi is a digital platform that facilitates the booking of employees on demand for temporary staffing services.Our services are available through our website and mobile apps (collectively, the "Platform").
+
+2. APPLICABILITY OF THE TERMS: 
+By accessing or using the Platform, you represent and warrant that you are authorized to enter into this binding agreement and agree to be bound by
+these terms of use ("Terms"), privacy policy, and cookies policy. If you do not agree to these Terms, do not use the Platform. We reserve the right to
+change these Terms at any time, so please check back regularly for updates. Your continued use of the Platform following the posting of any changes to these Terms will be deemed your acceptance of those changes.
+
+3. User eligibility: 
+The Platform is intended for users who are at least 18 years of age. By using the Platform, you represent and warrant that you are at least 18 years of age and have the legal capacity to enter into these Terms.
+
+4. Fees and payment:
+Rizorsi charges fees for its services on a fixed term or hourly basis, as stated at the time of booking. Fees are generally payable in advance, but Rizorsi may allow selected clients to book now and pay later,
+subject to an additional agreement. The applicable fee will be displayed on the Platform at the time of booking. You agree to pay all fees in a timely manner, and Rizorsi reserves the right to suspend or terminate your access to the Platform if you fail to pay any fees.
+
+5. Availability and booking of employees
+The availability of employees on the Platform is subject to change, and Rizorsi cannot guarantee the availability of any particular employee at any given time. You may book an employee through the Platform based on their availability as displayed on the Platform
+
+6. Cancellation policy
+No refunds will be provided for any cancellations made within 24 hours of the scheduled start time of the booked employee's shift. If you are eligible for a refund due to a cancellation made outside of the 24-hour window, any applicable fees charged by the payment processor will be your responsibility.
+
+7. Employee duties and responsibilities
+Employees who are booked through the Platform are expected to perform their duties to the best of their abilities and in accordance with any instructions or guidelines provided by the client. Rizorsi is not responsible for the performance or actions of any employee booked through the Platform.
+
+8. Termination
+Rizorsi reserves the right to terminate your access to the Platform at any time and for any reason, including, but not limited to, your breach of these Terms.
+
+9. Disclaimer of warranties
+THE PLATFORM IS PROVIDED "AS IS" AND "AS AVAILABLE" WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED. RIZORSI DISCLAIMS ALL WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. RIZORSI MAKES NO WARRANTY THAT THE PLATFORM WILL MEET YOUR REQUIREMENTS, OR THAT IT WILL BE UNINTERRUPTED, TIMELY, SECURE, OR ERROR-FREE.
+
+10. Limitation of liability
+IN NO EVENT SHALL RIZORSI BE LIABLE FOR ANY DAMAGES, INCLUDING, BUT NOT LIMITED TO, DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, OR ANY LOSS OF PROFITS OR REVENUES, WHETHER INCURRED DIRECTLY OR INDIRECTLY, OR ANY LOSS OF DATA, USE, GOODWILL, OR OTHER INTANGIBLE LOSSES, RESULTING FROM YOUR ACCESS TO OR USE OF THE PLATFORM.
+
+11. Indemnification
+You agree to indemnify and hold Rizorsi and its affiliates, officers, agents, and employees harmless from any claim or demand, including reasonable attorneys' fees, made by any third party due to or arising out of your use of the Platform, your violation of these Terms, or your violation of any rights of another.
+
+12.	Governing law
+These Terms and your use of the Platform shall be governed by and construed in accordance with the laws of Malta. Any disputes arising from or in connection with these Terms or the Platform shall be resolved through Maltese arbitration.
+
+13.	Severability
+If any provision of these Terms is found to be invalid or unenforceable, that provision shall be enforced to the maximum extent possible, and the remaining provisions shall remain in full force and effect.
+
+14.	Entire agreement
+These Terms constitute the entire agreement between you and Rizorsi regarding the use of the Platform, and supersede all prior or contemporaneous communications and proposals, whether oral or written.
+
+15. Contact us
+If you have any questions about these Terms, please contact us at "info@rizorsiumani.com.mt"
+
+
+`
